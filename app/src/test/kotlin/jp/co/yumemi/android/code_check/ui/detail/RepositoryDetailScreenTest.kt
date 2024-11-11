84b532ab6beee5060de7becce170962436dfe356
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-11 19:39:25 +0900

    test: language に何も設定されていなければ、言語自体表示されないことのテストを追加した。

--- app/src/test/kotlin/jp/co/yumemi/android/code_check/ui/detail/RepositoryDetailScreenTest.kt
+++ app/src/test/kotlin/jp/co/yumemi/android/code_check/ui/detail/RepositoryDetailScreenTest.kt
@@ -56,4 +56,25 @@ class RepositoryDetailScreenTest {
         rule.onNodeWithContentDescription("ownerIconUrl").assertDoesNotExist()
         rule.onNodeWithTag("language").assertExists()
     }
+
+    @Test
+    fun `language に何も設定されていなければ、言語自体表示されないこと`() {
+        rule.setContent {
+            RepositoryDetailScreen(
+                item = RepositoryItem(
+                    name = "dtrupenn/Tetris",
+                    ownerIconUrl = "https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
+                    language = null,
+                    stargazersCount = 1,
+                    watchersCount = 1,
+                    forksCount = 0,
+                    openIssuesCount = 0,
+                ),
+                onCloseButtonClick = {},
+            )
+        }
+
+        rule.onNodeWithContentDescription("ownerIconUrl").assertExists()
+        rule.onNodeWithTag("language").assertDoesNotExist()
+    }
 }
