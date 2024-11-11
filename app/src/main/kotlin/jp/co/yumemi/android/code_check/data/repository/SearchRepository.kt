7ae45b3657768ad241ad85cf5f5b6d730282dea4
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-11 09:26:13 +0900

    refactor: RepositorySearchViewModel#requestSearchRepositories をコピペした。

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/data/repository/SearchRepository.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/repository/SearchRepository.kt
@@ -1,4 +1,21 @@
 package jp.co.yumemi.android.code_check.data.repository
 
-class SearchRepository {
+import io.ktor.client.HttpClient
+import io.ktor.client.call.receive
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.parameter
+import io.ktor.client.statement.HttpResponse
+import javax.inject.Inject
+
+class SearchRepository @Inject constructor(
+    private val client: HttpClient,
+) {
+    suspend fun requestSearchRepositories(inputText: String): String {
+        val response: HttpResponse = client.get("https://api.github.com/search/repositories") {
+            header("Accept", "application/vnd.github.v3+json")
+            parameter("q", inputText)
+        }
+        return response.receive()
+    }
 }
