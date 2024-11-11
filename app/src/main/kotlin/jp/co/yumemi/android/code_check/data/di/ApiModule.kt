a8580216c45be250ba81eb495f163a183b49e1da
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-11 09:05:53 +0900

    refactor: API 関連の DI を注入するための ApiModule を作成した。

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/di/ApiModule.kt
@@ -0,0 +1,9 @@
+package jp.co.yumemi.android.code_check.data.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+object ApiModule
