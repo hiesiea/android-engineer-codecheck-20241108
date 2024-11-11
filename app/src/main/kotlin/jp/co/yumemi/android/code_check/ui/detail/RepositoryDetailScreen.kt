ecdccbe3bc96c432665fc7854f973e06dd753ced
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-11 17:59:00 +0900

    refactor: RepositoryDetailFragment 用の UI を定義するための RepositoryDetailScreen を定義した。

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/detail/RepositoryDetailScreen.kt
@@ -0,0 +1,15 @@
+package jp.co.yumemi.android.code_check.ui.detail
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RepositoryDetailScreen(modifier: Modifier = Modifier) {
+}
+
+@Preview
+@Composable
+private fun RepositoryDetailScreenPreview() {
+    RepositoryDetailScreen()
+}
