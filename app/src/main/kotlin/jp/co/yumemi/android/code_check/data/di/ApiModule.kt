e6374d89cadd26733380a418f30912759effa18e
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-11 10:55:02 +0900

    chore: API からの JSON レスポンスを扱うための設定を追加した。

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/data/di/ApiModule.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/di/ApiModule.kt
@@ -6,7 +6,10 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
 import jp.co.yumemi.android.code_check.data.repository.SearchRepository
+import kotlinx.serialization.json.Json
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -15,7 +18,15 @@ object ApiModule {
     @Provides
     @Singleton
     fun provideHttpClient(): HttpClient {
-        return HttpClient(Android)
+        return HttpClient(Android) {
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        ignoreUnknownKeys = true
+                    },
+                )
+            }
+        }
     }
 
     @Provides
