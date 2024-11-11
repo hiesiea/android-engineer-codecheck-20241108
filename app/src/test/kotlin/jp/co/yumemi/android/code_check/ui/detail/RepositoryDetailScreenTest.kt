944619f538b28a0f97f9ba3c6ad1fee36f7e8be7
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-11 19:19:37 +0900

    test: RepositoryDetailScreenTest の雛形を作成した。

---
+++ app/src/test/kotlin/jp/co/yumemi/android/code_check/ui/detail/RepositoryDetailScreenTest.kt
@@ -0,0 +1,12 @@
+package jp.co.yumemi.android.code_check.ui.detail
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class RepositoryDetailScreenTest {
+    @get:Rule
+    val rule = createComposeRule()
+}
