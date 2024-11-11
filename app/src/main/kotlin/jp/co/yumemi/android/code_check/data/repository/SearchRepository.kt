92cc6289726d1618965e872f167ef1c835b531a6
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-11 11:11:12 +0900

    refactor: header 指定はなくても結果は変わらないようだったので削除した。

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/data/repository/SearchRepository.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/repository/SearchRepository.kt
@@ -3,7 +3,6 @@ package jp.co.yumemi.android.code_check.data.repository
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
-import io.ktor.client.request.header
 import io.ktor.client.request.parameter
 import io.ktor.client.statement.HttpResponse
 import jp.co.yumemi.android.code_check.data.model.SearchRepositoriesResponse
@@ -14,7 +13,6 @@ class SearchRepository @Inject constructor(
 ) {
     suspend fun requestSearchRepositories(inputText: String): SearchRepositoriesResponse {
         val response: HttpResponse = client.get("https://api.github.com/search/repositories") {
-            header("Accept", "application/vnd.github.v3+json")
             parameter("q", inputText)
         }
         return response.body<SearchRepositoriesResponse>()
