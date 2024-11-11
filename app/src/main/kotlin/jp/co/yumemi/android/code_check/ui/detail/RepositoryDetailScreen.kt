0458b11d070f13028e3d985be058fa6e4495868d
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-11 18:21:27 +0900

    refactor: RepositoryDetailScreen に TopAppBar を追加しつつ、変更がわかりやすいよう MainTheme で囲む。

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/detail/RepositoryDetailScreen.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/detail/RepositoryDetailScreen.kt
@@ -2,20 +2,46 @@ package jp.co.yumemi.android.code_check.ui.detail
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import jp.co.yumemi.android.code_check.data.model.RepositoryItem
+import jp.co.yumemi.android.code_check.ui.theme.MainTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RepositoryDetailScreen(
     item: RepositoryItem,
+    onCloseButtonClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Scaffold(
         modifier = modifier,
-        topBar = {},
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = item.name) },
+                navigationIcon = {
+                    IconButton(
+                        onClick = onCloseButtonClick,
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Close,
+                            tint = Color.Black,
+                            contentDescription = null,
+                        )
+                    }
+                },
+            )
+        },
     ) {
         Column(modifier = Modifier.padding(it)) {
 
@@ -26,15 +52,18 @@ fun RepositoryDetailScreen(
 @Preview
 @Composable
 private fun RepositoryDetailScreenPreview() {
-    RepositoryDetailScreen(
-        item = RepositoryItem(
-            name = "dtrupenn/Tetris",
-            ownerIconUrl = "https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
-            language = "Assembly",
-            stargazersCount = 1,
-            watchersCount = 1,
-            forksCount = 0,
-            openIssuesCount = 0,
+    MainTheme {
+        RepositoryDetailScreen(
+            item = RepositoryItem(
+                name = "dtrupenn/Tetris",
+                ownerIconUrl = "https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
+                language = "Assembly",
+                stargazersCount = 1,
+                watchersCount = 1,
+                forksCount = 0,
+                openIssuesCount = 0,
+            ),
+            onCloseButtonClick = {},
         )
-    )
+    }
 }
