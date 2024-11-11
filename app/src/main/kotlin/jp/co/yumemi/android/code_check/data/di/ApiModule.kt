7f40d04089c25bfe4740c1a428f6f9810a195c89
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-11 09:43:54 +0900

    refactor: ApiModule に provideSearchRepository を追加した。

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/data/di/ApiModule.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/di/ApiModule.kt
@@ -6,6 +6,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
+import jp.co.yumemi.android.code_check.data.repository.SearchRepository
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -15,5 +16,13 @@ object ApiModule {
     @Singleton
     fun provideHttpClient(): HttpClient {
         return HttpClient(Android)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSearchRepository(
+        httpClient: HttpClient,
+    ): SearchRepository {
+        return SearchRepository(httpClient)
     }
 }
