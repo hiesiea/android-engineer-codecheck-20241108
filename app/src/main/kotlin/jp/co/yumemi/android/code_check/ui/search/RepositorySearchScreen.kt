8f17fcac18aa00ab4afd4607fe41844adc4ac568
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-12 09:58:59 +0900

    refactor: RepositorySearchScreen.kt を新規作成。

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/search/RepositorySearchScreen.kt
@@ -0,0 +1,15 @@
+package jp.co.yumemi.android.code_check.ui.search
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RepositorySearchScreen(modifier: Modifier = Modifier) {
+}
+
+@Preview
+@Composable
+private fun RepositorySearchScreenPreview() {
+    RepositorySearchScreen()
+}
