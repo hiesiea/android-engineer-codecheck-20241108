37693bf8e2e528d4a66ef7e2cd0a539c55c45c3f
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-12 16:00:44 +0900

    test: RepositorySearchScreenTest の雛形を作成した。

---
+++ app/src/test/kotlin/jp/co/yumemi/android/code_check/ui/search/RepositorySearchScreenTest.kt
@@ -0,0 +1,13 @@
+package jp.co.yumemi.android.code_check.ui.search
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@Suppress("NonAsciiCharacters")
+@RunWith(RobolectricTestRunner::class)
+class RepositorySearchScreenTest {
+    @get:Rule
+    val rule = createComposeRule()
+}
