77cccf6787359e56cc2ac5b2da4ee34ec5e9958b
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-12 18:01:20 +0900

    test: ユニットテストに失敗していたのを修正した。

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/data/model/RepositoryItem.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/model/RepositoryItem.kt
@@ -19,7 +19,7 @@ data class RepositoryItem(
             return RepositoryItem(
                 name = "dtrupenn/Tetris",
                 ownerIconUrl = "https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
-                htmlUrl = "https://api.github.com/repos/dtrupenn/Tetris",
+                htmlUrl = "https://github.com/dtrupenn/Tetris",
                 language = "Assembly",
                 stargazersCount = 1,
                 watchersCount = 1,
--- app/src/main/kotlin/jp/co/yumemi/android/code_check/data/model/SearchRepositoriesResponse.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/model/SearchRepositoriesResponse.kt
@@ -32,7 +32,7 @@ fun SearchRepositoryResponse.Companion.fake(): SearchRepositoryResponse {
     return SearchRepositoryResponse(
         fullName = "dtrupenn/Tetris",
         owner = Owner(avatarUrl = "https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png"),
-        htmlUrl = "https://api.github.com/repos/dtrupenn/Tetris",
+        htmlUrl = "https://github.com/dtrupenn/Tetris",
         language = "Assembly",
         stargazersCount = 1,
         watchersCount = 1,
