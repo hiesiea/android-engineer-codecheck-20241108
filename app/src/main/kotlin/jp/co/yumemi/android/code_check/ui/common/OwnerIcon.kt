e46be51cc1f671c0452d447d222c32cd0c7113b4
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-12 15:21:52 +0900

    fix: アイコン URL が不正だった時にグレーの PNG 画像を表示していたが、APK のリソースを食うのと CI が失敗してしまうので、グレーの Box を表示する方式に切り替えた。

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/common/OwnerIcon.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/common/OwnerIcon.kt
@@ -1,8 +1,15 @@
 package jp.co.yumemi.android.code_check.ui.common
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,6 +21,8 @@ fun OwnerIcon(
     ownerIconUrl: String,
     modifier: Modifier = Modifier,
 ) {
+    var isError by remember { mutableStateOf(false) }
+
     if (LocalInspectionMode.current) {
         // Preview 時
         Image(
@@ -23,12 +32,18 @@ fun OwnerIcon(
         )
     } else {
         // 通常時
-        AsyncImage(
-            model = ownerIconUrl,
-            contentDescription = "ownerIconUrl",
-            modifier = modifier,
-            error = painterResource(id = R.drawable.empty_image),
-        )
+        if (isError) {
+            Box(modifier = modifier.background(Color.LightGray))
+        } else {
+            AsyncImage(
+                model = ownerIconUrl,
+                contentDescription = "ownerIconUrl",
+                modifier = modifier,
+                onError = {
+                    isError = true
+                },
+            )
+        }
     }
 }
 
