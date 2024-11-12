56c572570e2ec178d1d41e47722bcb98a737b399
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-12 17:33:02 +0900

    chore: 不要になったライブラリの削除と、Custom Tabs のライブラリを追加した。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.browser:browser:1.8.0")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
@@ -75,7 +76,6 @@ dependencies {
 
     implementation("org.slf4j:slf4j-android:1.7.36")
 
-    implementation("io.coil-kt:coil:1.3.2")
     implementation("io.coil-kt:coil-compose:2.7.0")
 
     implementation("com.jakewharton.timber:timber:5.0.1")
