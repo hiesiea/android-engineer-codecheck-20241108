4e9e75b35e94d87220dc1170f6e19f049175647a
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-12 10:48:35 +0900

    refactor: RepositorySearchScreen で表示するアイテムの UI を実装した。

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/search/RepositorySearchScreen.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/search/RepositorySearchScreen.kt
@@ -1,15 +1,30 @@
 package jp.co.yumemi.android.code_check.ui.search
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import jp.co.yumemi.android.code_check.R
 import jp.co.yumemi.android.code_check.data.model.RepositoryItem
 
 @Composable
@@ -32,8 +47,49 @@ fun RepositorySearchScreen(
         LazyColumn(
             modifier = Modifier.padding(it),
         ) {
-            items(repositoryItems) {
-
+            items(repositoryItems) { item ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(all = 8.dp),
+                ) {
+                    val imageModifier = Modifier.size(80.dp)
+                    if (LocalInspectionMode.current) {
+                        // Preview 時
+                        Image(
+                            painter = painterResource(R.drawable.jetbrains),
+                            contentDescription = "ownerIconUrl",
+                            modifier = imageModifier,
+                        )
+                    } else {
+                        // 通常時
+                        AsyncImage(
+                            model = item.ownerIconUrl,
+                            contentDescription = "ownerIconUrl",
+                            modifier = imageModifier,
+                            error = painterResource(id = R.drawable.empty_image),
+                        )
+                    }
+                    Column {
+                        Text(
+                            text = item.name,
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold,
+                            overflow = TextOverflow.Ellipsis,
+                            maxLines = 2,
+                        )
+                        item.language?.let { language ->
+                            Text(
+                                text = stringResource(R.string.written_language, language),
+                                modifier = Modifier
+                                    .padding(vertical = 4.dp)
+                                    .testTag("language"),
+                            )
+                        }
+                        Text(text = stringResource(R.string.stars_count, item.stargazersCount))
+                    }
+                }
+                HorizontalDivider()
             }
         }
     }
