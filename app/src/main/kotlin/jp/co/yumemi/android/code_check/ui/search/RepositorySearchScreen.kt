7f12e9528a713bb2a5cf9b170a176b6da65e03ab
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-12 10:11:05 +0900

    refactor: topBar に検索用のテキストフィールドを追加した。

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/search/RepositorySearchScreen.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/search/RepositorySearchScreen.kt
@@ -1,12 +1,15 @@
 package jp.co.yumemi.android.code_check.ui.search
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import jp.co.yumemi.android.code_check.data.model.RepositoryItem
 
 @Composable
@@ -17,7 +20,13 @@ fun RepositorySearchScreen(
     Scaffold(
         modifier = modifier,
         topBar = {
-
+            TextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(all = 12.dp),
+            )
         },
     ) {
         LazyColumn(
