96a9e7af4594f86d618c6e2ed4d656e7234810b9
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-12 15:07:54 +0900

    refactor: オーナーアイコンについて、処理が複雑になりそうで別ファイル化したほうがよさそうだったので分割のための準備を進める。

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/common/OwnerIcon.kt
@@ -0,0 +1,15 @@
+package jp.co.yumemi.android.code_check.ui.common
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun OwnerIcon(modifier: Modifier = Modifier) {
+}
+
+@Preview
+@Composable
+private fun OwnerIconPreview() {
+    OwnerIcon()
+}
