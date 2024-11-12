88faeb43a1501a42f6d4bd669b6bb140b6e12794
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-12 10:05:36 +0900

    refactor: RepositorySearchScreen のリスト部分の大枠を作成した。

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/search/RepositorySearchScreen.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/search/RepositorySearchScreen.kt
@@ -1,15 +1,49 @@
 package jp.co.yumemi.android.code_check.ui.search
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import jp.co.yumemi.android.code_check.data.model.RepositoryItem
 
 @Composable
-fun RepositorySearchScreen(modifier: Modifier = Modifier) {
+fun RepositorySearchScreen(
+    repositoryItems: List<RepositoryItem>,
+    modifier: Modifier = Modifier,
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+
+        },
+    ) {
+        LazyColumn(
+            modifier = Modifier.padding(it),
+        ) {
+            items(repositoryItems) {
+
+            }
+        }
+    }
 }
 
 @Preview
 @Composable
 private fun RepositorySearchScreenPreview() {
-    RepositorySearchScreen()
+    RepositorySearchScreen(
+        repositoryItems = List(10) {
+            RepositoryItem(
+                name = "dtrupenn/Tetris",
+                ownerIconUrl = "https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
+                language = "Assembly",
+                stargazersCount = 1,
+                watchersCount = 1,
+                forksCount = 0,
+                openIssuesCount = 0,
+            )
+        },
+    )
 }
