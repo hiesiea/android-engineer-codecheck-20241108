abcf9bc92bd0c8bb0c4e8971599e7b65cd6756da
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-13 12:15:36 +0900

    fix: タイトルが長すぎる場合に TopAppBar が2行表示になってしまっていたので、1行で表示できるようにした。

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/detail/RepositoryDetailScreen.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/detail/RepositoryDetailScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import jp.co.yumemi.android.code_check.R
@@ -37,7 +38,13 @@ fun RepositoryDetailScreen(
         modifier = modifier,
         topBar = {
             CenterAlignedTopAppBar(
-                title = { Text(text = item.name) },
+                title = {
+                    Text(
+                        text = item.name,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                    )
+                },
                 navigationIcon = {
                     IconButton(
                         onClick = onCloseButtonClick,
