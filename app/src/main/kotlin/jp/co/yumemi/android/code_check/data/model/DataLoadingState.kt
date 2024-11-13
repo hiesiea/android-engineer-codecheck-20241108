df547e3a5a435868d57b765c3686661ac474a9de
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-13 09:31:11 +0900

    feat: データの読み込み状態を表すための sealed class を作成した。

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/data/model/DataLoadingState.kt
@@ -0,0 +1,15 @@
+package jp.co.yumemi.android.code_check.data.model
+
+/**
+ * データの読み込み状態を表す
+ */
+sealed class DataLoadingState {
+    /** 読み込み中 */
+    data object InProgress : DataLoadingState()
+
+    /** 読み込み成功 */
+    data object Success : DataLoadingState()
+
+    /** 読み込み失敗 */
+    data class Failure(val throwable: Throwable) : DataLoadingState()
+}
