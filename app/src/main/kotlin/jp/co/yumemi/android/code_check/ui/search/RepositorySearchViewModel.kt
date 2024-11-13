c5d739768527dc188bfaedc99f031d8b92840672
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-13 10:17:35 +0900

    test: uiState に切り替えたため、それに合わせてテストを切り替えた。

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/search/RepositorySearchViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/search/RepositorySearchViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jp.co.yumemi.android.code_check.data.model.DataLoadingState
-import jp.co.yumemi.android.code_check.data.model.RepositoryItem
 import jp.co.yumemi.android.code_check.data.model.toRepositoryItem
 import jp.co.yumemi.android.code_check.data.repository.SearchRepository
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -21,9 +20,6 @@ import javax.inject.Inject
 class RepositorySearchViewModel @Inject constructor(
     private val searchRepository: SearchRepository,
 ) : ViewModel() {
-    private val _repositoryItems = MutableStateFlow<List<RepositoryItem>>(emptyList())
-    val repositoryItems: StateFlow<List<RepositoryItem>> = _repositoryItems.asStateFlow()
-
     private val _uiState = MutableStateFlow(RepositorySearchUiState())
     val uiState: StateFlow<RepositorySearchUiState> = _uiState.asStateFlow()
 
@@ -35,16 +31,15 @@ class RepositorySearchViewModel @Inject constructor(
     fun searchRepositories(inputText: String) = viewModelScope.launch {
         try {
             val response = searchRepository.requestSearchRepositories(inputText = inputText)
-            _repositoryItems.value = response.items.map { it.toRepositoryItem() }
             _uiState.value = RepositorySearchUiState(
                 dataLoadingState = DataLoadingState.Success,
                 repositoryItems = response.items.map { it.toRepositoryItem() },
             )
         } catch (throwable: Throwable) {
             Timber.e(throwable)
-            _repositoryItems.value = emptyList()
             _uiState.value = RepositorySearchUiState(
                 dataLoadingState = DataLoadingState.Failure(throwable = throwable),
+                repositoryItems = emptyList(),
             )
         }
     }
