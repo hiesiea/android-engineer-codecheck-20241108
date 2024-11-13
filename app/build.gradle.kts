1e70f82ca7b105ccc2a9f4435ca628d9abb6a46e
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-11-13 19:40:26 +0900

    refactor: dependencies の順番を整理した。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,50 +50,37 @@ dependencies {
     implementation(platform(libs.compose.bom))
     implementation(libs.compose.material3)
     implementation(libs.compose.ui.tooling.preview)
-    debugImplementation(libs.compose.ui.tooling)
-    debugImplementation(libs.compose.ui.test.manifest)
-
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.constraintlayout)
     implementation(libs.browser)
-
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.lifecycle.livedata.ktx)
     implementation(libs.lifecycle.runtime.ktx)
-
     implementation(libs.navigation.fragment.ktx)
     implementation(libs.navigation.ui.ktx)
-
     implementation(libs.kotlinx.coroutines.android)
-
     implementation(libs.ktor.client.android)
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.client.logging)
-
     implementation(libs.slf4j.android)
-
     implementation(libs.coil.compose)
-
     implementation(libs.timber)
-
     implementation(libs.hilt.android)
+
+    debugImplementation(libs.compose.ui.tooling)
+    debugImplementation(libs.compose.ui.test.manifest)
+
     kapt(libs.hilt.compiler)
 
     testImplementation(libs.junit)
-
     testImplementation(libs.robolectric)
-
     testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.kotlin.test)
-
     testImplementation(libs.ktor.client.mock)
-
     testImplementation(libs.mockito.kotlin)
-
     testImplementation(libs.turbine)
-
     testImplementation(libs.compose.ui.test.junit4)
 
     androidTestImplementation(libs.ext.junit)
