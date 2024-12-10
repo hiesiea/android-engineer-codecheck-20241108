36f0b744f547d1164a7296b6d8ec43ed2ddaafd3
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-12-11 07:55:20 +0900

    refactor: DataLoadingState について、class である必要はなさそうだったので interface に変更した。

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/data/model/DataLoadingState.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/data/model/DataLoadingState.kt
@@ -3,16 +3,16 @@ package jp.co.yumemi.android.codecheck.data.model
 /**
  * データの読み込み状態を表す
  */
-sealed class DataLoadingState {
+sealed interface DataLoadingState {
     /** 初回表示時 */
-    data object Initial : DataLoadingState()
+    data object Initial : DataLoadingState
 
     /** 読み込み中 */
-    data object InProgress : DataLoadingState()
+    data object InProgress : DataLoadingState
 
     /** 読み込み成功 */
-    data object Success : DataLoadingState()
+    data object Success : DataLoadingState
 
     /** 読み込み失敗 */
-    data class Failure(val errorType: ErrorType) : DataLoadingState()
+    data class Failure(val errorType: ErrorType) : DataLoadingState
 }
