9d8c309af4c818090a9e51d7eba52d9a4ae28d09
Hitoshi KAMADA <kmd.htsh@gmail.com>
2024-12-11 22:18:16 +0900

    test: RepositoryDetailScreenTest にて、以下の対応を行った。
    * fake 関数を使用するのをやめる。
    * OwnerIcon は常に表示されているので、OwnerIcon のテストは削除した。

--- app/src/test/kotlin/jp/co/yumemi/android/codecheck/ui/detail/RepositoryDetailScreenTest.kt
+++ app/src/test/kotlin/jp/co/yumemi/android/codecheck/ui/detail/RepositoryDetailScreenTest.kt
@@ -1,7 +1,6 @@
 package jp.co.yumemi.android.codecheck.ui.detail
 
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import jp.co.yumemi.android.codecheck.data.model.RepositoryItem
 import org.junit.Rule
@@ -16,16 +15,24 @@ class RepositoryDetailScreenTest {
     val rule = createComposeRule()
 
     @Test
-    fun `ownerIconUrl や language に何か設定されていれば表示されること`() {
+    fun `language に何か設定されていれば表示されること`() {
         rule.setContent {
             RepositoryDetailScreen(
-                item = RepositoryItem.fake(),
+                item = RepositoryItem(
+                    name = "dtrupenn/Tetris",
+                    ownerIconUrl = "https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
+                    htmlUrl = "https://github.com/dtrupenn/Tetris",
+                    language = "Assembly",
+                    stargazersCount = 1,
+                    watchersCount = 1,
+                    forksCount = 0,
+                    openIssuesCount = 0,
+                ),
                 onCloseButtonClick = {},
                 onShowDetailButtonClick = {},
             )
         }
 
-        rule.onNodeWithContentDescription("OwnerIcon").assertExists()
         rule.onNodeWithTag("language").assertExists()
     }
 
@@ -33,13 +40,21 @@ class RepositoryDetailScreenTest {
     fun `language に何も設定されていなければ、言語自体表示されないこと`() {
         rule.setContent {
             RepositoryDetailScreen(
-                item = RepositoryItem.fake().copy(language = null),
+                item = RepositoryItem(
+                    name = "dtrupenn/Tetris",
+                    ownerIconUrl = "https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
+                    htmlUrl = "https://github.com/dtrupenn/Tetris",
+                    language = null,
+                    stargazersCount = 1,
+                    watchersCount = 1,
+                    forksCount = 0,
+                    openIssuesCount = 0,
+                ),
                 onCloseButtonClick = {},
                 onShowDetailButtonClick = {},
             )
         }
 
-        rule.onNodeWithContentDescription("OwnerIcon").assertExists()
         rule.onNodeWithTag("language").assertDoesNotExist()
     }
 }
