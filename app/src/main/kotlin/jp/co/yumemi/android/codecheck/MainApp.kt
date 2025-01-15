654234bcd4491d3b40bc485d9ae30531e366c51d
Hitoshi KAMADA <kmd.htsh@gmail.com>
2025-01-15 22:31:13 +0900

    refactor: App コンポーザブル内にアプリの NavHost を作成し、navController を渡す。

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/MainApp.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/MainApp.kt
@@ -1,9 +1,21 @@
 package jp.co.yumemi.android.codecheck
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import jp.co.yumemi.android.codecheck.feature.search.SearchRoute
 
 @Composable
 fun MainApp() {
     val navController = rememberNavController()
+    MainNavHost(navController = navController)
+}
+
+@Composable
+fun MainNavHost(
+    navController: NavHostController,
+) {
+    NavHost(navController = navController, startDestination = SearchRoute) {
+    }
 }
