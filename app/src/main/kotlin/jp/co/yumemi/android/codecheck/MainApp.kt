590998a2e78bf86fa3a14f20cee4bed736369387
Hitoshi KAMADA <kmd.htsh@gmail.com>
2025-01-15 21:52:13 +0900

    refactor: App-level Composable を作成し、Compose エントリーポイントとして Activity に追加して、View Layout の設定を置き換えた。

---
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/MainApp.kt
@@ -0,0 +1,8 @@
+package jp.co.yumemi.android.codecheck
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainApp(modifier: Modifier = Modifier) {
+}
