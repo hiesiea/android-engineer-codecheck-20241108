34b3e406acf31faeea0a12cb263e983c24a02c6c
Hitoshi KAMADA <kmd.htsh@gmail.com>
2025-01-15 21:44:48 +0900

    chore: navigation-compose ライブラリを導入した。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,5 +51,6 @@ dependencies {
     implementation(libs.browser)
     implementation(libs.navigation.fragment.ktx)
     implementation(libs.navigation.ui.ktx)
+    implementation(libs.navigation.compose)
     implementation(libs.slf4j.android)
 }
