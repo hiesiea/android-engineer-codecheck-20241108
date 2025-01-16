c985c8fe010863127bc014329c76f0cc983487a6
Hitoshi KAMADA <kmd.htsh@gmail.com>
2025-01-16 22:31:46 +0900

    refactor: MainNavHost に RepositorySearchScreen を追加した。

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/MainApp.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/MainApp.kt
@@ -3,7 +3,10 @@ package jp.co.yumemi.android.codecheck
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import jp.co.yumemi.android.codecheck.feature.detail.DetailRoute
+import jp.co.yumemi.android.codecheck.feature.search.RepositorySearchScreen
 import jp.co.yumemi.android.codecheck.feature.search.SearchRoute
 
 @Composable
@@ -17,5 +20,23 @@ fun MainNavHost(
     navController: NavHostController,
 ) {
     NavHost(navController = navController, startDestination = SearchRoute) {
+        composable<SearchRoute> {
+            RepositorySearchScreen(
+                onItemClick = {
+                    navController.navigate(
+                        DetailRoute(
+                            name = it.name,
+                            ownerIconUrl = it.ownerIconUrl,
+                            htmlUrl = it.htmlUrl,
+                            language = it.language,
+                            stargazersCount = it.stargazersCount,
+                            watchersCount = it.watchersCount,
+                            forksCount = it.forksCount,
+                            openIssuesCount = it.openIssuesCount,
+                        ),
+                    )
+                },
+            )
+        }
     }
 }
